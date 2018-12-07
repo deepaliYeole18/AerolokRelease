fd96799181e8fe095e83c50006ff67ca531d17ba
bhalgat20 <bhalgatashish@gmail.com>
2018-12-07 10:59:03 +0000

    Add project files.

---
+++ WebApplication3/Controllers/ValuesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            var splitpath = value.Split(',');
+            var data = InvokeRequestResponseService(splitpath[1]).Result;
+            return data;
+        }
+
+        static async Task<string> InvokeRequestResponseService(string path)
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+                    Inputs = new Dictionary<string, List<Dictionary<string, string>>>() {
+                        {
+                            "input1",
+                            new List<Dictionary<string, string>>(){new Dictionary<string, string>(){
+                                            {
+                                                "Col1", path
+                                            },
+                                }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+                const string apiKey = "8rvLv0bDu7D0Z2K2ExkAs1CGS3yTt8FvSUMiaRMZRFB1PvORuw0uXrwP5Kz24t3U9aCTZXjAjZ3aUwGIbSPEWA=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/subscriptions/55de3f089bfd4719922fb7abc02a2a95/services/52607654d9674a7492c3654101dc25b4/execute?api-version=2.0&format=swagger");
+
+                // WARNING: The 'await' statement below can result in a deadlock
+                // if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false)
+                // so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+                    var jo = JObject.Parse(result);
+                    var res = jo.ToString();
+                    dynamic stuff = JsonConvert.DeserializeObject(res);
+                    var data2 = stuff.Results.output1[0].Graphics;
+                    return data2;
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp,
+                    // which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                    var jo = JObject.Parse(responseContent);
+                    var res = jo.ToString();
+                    dynamic stuff = JsonConvert.DeserializeObject(res);
+                    var data2 = stuff.error.message;
+                    return responseContent;
+                }
+            }
+        }
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
